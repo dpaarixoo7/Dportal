f18327e3461e86d304e8a6dfa068d9f42a7cf77c
Paritosh Pandey <paritoshp73@gmail.com>
2015-09-29 11:10:34 +0530

    Update config.php

--- config.php
+++ config.php
@@ -10,4 +10,14 @@ $db->uname = 'db username';
 $db->pass = 'password';
 $db->db_name = 'database name';
 
+
+//retrieve database credentials
+$DB_HOST = $db->host;
+$DB_USERNAME = $db->uname;
+$DB_PASSWORD = $db->pass;
+$DB_DATABASE = $db->db_name;
+
+//database connection
+$connection = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD);
+
 ?>
