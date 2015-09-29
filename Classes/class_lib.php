c8f1f9fbd931567a9e24182b1f41f402e65b3d86
Paritosh Pandey <paritoshp73@gmail.com>
2015-09-29 11:56:34 +0530

    Update class_lib.php

--- Classes/class_lib.php
+++ Classes/class_lib.php
@@ -3,28 +3,10 @@
 // database connection class
 class database_credentials {
   
-  private $host;
-  private $uname;
-  private $pass;
-  private $db_name;
-  
-  function set_db_credentials($arg1, $arg2, $arg3, $arg4) {
-    
-    $this->host = $arg1;
-    $this->uname = $arg2;
-    $this->pass = $arg3;
-    $this->db_name = $arg4;
-    
-  }
-  
-  function get_db_credentials() {
-    
-    $this->host;
-    $this->uname;
-    $this->pass;
-    $this->db_name;
-    
-  }
+  public $host;
+  public $uname;
+  public $pass;
+  public $db_name;
   
 }
 
