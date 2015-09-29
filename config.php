ebee9e6ccd87d5a75b4142796b75604ecbbb1265
Paritosh Pandey <paritoshp73@gmail.com>
2015-09-29 11:30:21 +0530

    Update config.php

--- config.php
+++ config.php
@@ -20,4 +20,9 @@ $DB_DATABASE = $db->db_name;
 //database connection
 $connection = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD);
 
+//check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
 ?>
