9b0f9df5f19e957897037df76c2ef19063afac3c
Paritosh Pandey <paritoshp73@gmail.com>
2015-09-29 09:54:32 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 
+include('Classes/class_lib.php');
+
 // here we set database creditials
-define( "DB_HOST", "localhost" );
-define ("DB_NAME", "databasename");
-define( "DB_USERNAME", "username" );
-define( "DB_PASSWORD", "password" );
+$db = new database_credentials;
+
+$db->host = 'localhost';
+$db->uname = 'db username';
+$db->pass = 'password';
+$db->db_name = 'database name';
 
 ?>
