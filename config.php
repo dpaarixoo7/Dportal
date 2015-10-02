dc5ee5134da32462f180937b961b2c7dbc06cb2e
Paritosh Pandey <paritoshp73@gmail.com>
2015-10-02 15:11:06 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,28 +1,9 @@
 <?php
 
-include('Classes/class_lib.php');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'dportal');
 
-// here we set database creditials
-$db = new database_credentials;
-
-$db->host = 'localhost';
-$db->uname = 'db username';
-$db->pass = 'password';
-$db->db_name = 'database name';
-
-
-//retrieve database credentials
-$DB_HOST = $db->host;
-$DB_USERNAME = $db->uname;
-$DB_PASSWORD = $db->pass;
-$DB_DATABASE = $db->db_name;
-
-//database connection
-$connection = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD);
-
-//check connection
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
 
 ?>
