3a4f2545c50f65381b86041ab4565b0ba33af31f
Paritosh Pandey <paritoshp73@gmail.com>
2015-10-02 14:54:12 +0530

    Create choose.php

---
+++ choose.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<center>
+<table style="font-family: sans-serif; width: 770px; margin-top: 120px;">
+  <tbody>
+    <tr>
+      <td style="border-right: 1px solid gainsboro;">
+        <p style="text-align: center; font-size: 35px;">Teacher's Section</p>
+		<center>
+		<a href="login-teacher.php" style="text-decoration: none;">
+  <div style="background-color: chartreuse; height: 50px; width: 150px; border-radius: 8px; cursor: pointer;">
+  <p style="margin-left: 5px; padding-top: 15px; font-family: sans-serif;">Login</p></div></a><br>
+  <a href="register-teacher.php" style="text-decoration: none;">
+  <div style="background-color: chartreuse; height: 50px; width: 150px; border-radius: 8px; cursor: pointer;">
+  <p style="margin-left: 5px; padding-top: 15px; font-family: sans-serif;">Register Now</p></div></a>
+		</center>
+      </td>
+      <td>
+        <p style="text-align: center; font-size: 35px;">Student's Section</p>
+				<center>
+		<a href="login-student.php" style="text-decoration: none;">
+  <div style="background-color: chartreuse; height: 50px; width: 150px; border-radius: 8px; cursor: pointer;">
+  <p style="margin-left: 5px; padding-top: 15px; font-family: sans-serif;">Login</p></div></a><br>
+  <a href="register-student.php" style="text-decoration: none;">
+  <div style="background-color: chartreuse; height: 50px; width: 150px; border-radius: 8px; cursor: pointer;">
+  <p style="margin-left: 5px; padding-top: 15px; font-family: sans-serif;">Register Now</p></div></a>
+		</center>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</center>
+
+</body>
+</html>
