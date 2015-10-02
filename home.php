df61848fc99181fe5155256fb17d24f0edfa4830
Paritosh Pandey <paritoshp73@gmail.com>
2015-10-02 14:55:05 +0530

    Update home.php

--- home.php
+++ home.php
@@ -7,20 +7,11 @@
 <center>
 <h1 style="font-size: 120px;">DPortal</h1>
 <h3 style="font-size: 50px;">Home</h3>
-<table>
-<tr>
-<td>
-<a href="login.php" style="text-decoration: none;">
+<br>
+<br>
+<a href="choose.php" style="text-decoration: none;">
   <div style="background-color: chartreuse; height: 50px; width: 150px; border-radius: 8px; cursor: pointer;">
-  <p style="margin-left: 55px; padding-top: 15px; font-family: sans-serif;">Login</p></div></a>
-</td>
-<td>
-<a href="registration.php" style="text-decoration: none;">
-  <div style="background-color: chartreuse; height: 50px; width: 200px; border-radius: 8px; cursor: pointer;">
-  <p style="margin-left: 55px; padding-top: 15px; font-family: sans-serif;">Register Now</p></div></a>
-</td>
-</tr>
-</table>
+  <p style="margin-left: 5px; padding-top: 15px; font-family: sans-serif;">Enter</p></div></a>
 </center>
 
 </body>
