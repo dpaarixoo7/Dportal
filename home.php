921e678465a80ef4bb6c572fdccc2b1cfe0d4af4
Paritosh Pandey <paritoshp73@gmail.com>
2015-10-02 14:17:50 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<center>
+<h1 style="font-size: 120px;">DPortal</h1>
+<h3 style="font-size: 50px;">Home</h3>
+<table>
+<tr>
+<td>
+<a href="login.php" style="text-decoration: none;">
+  <div style="background-color: chartreuse; height: 50px; width: 150px; border-radius: 8px; cursor: pointer;">
+  <p style="margin-left: 55px; padding-top: 15px; font-family: sans-serif;">Login</p></div></a>
+</td>
+<td>
+<a href="registration.php" style="text-decoration: none;">
+  <div style="background-color: chartreuse; height: 50px; width: 200px; border-radius: 8px; cursor: pointer;">
+  <p style="margin-left: 55px; padding-top: 15px; font-family: sans-serif;">Register Now</p></div></a>
+</td>
+</tr>
+</table>
+</center>
+
+</body>
+</html>
